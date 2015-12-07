78c7eedda58bd449886dddedd893692d5d403d14
Connor Weng <weng@icbc.com>
2015-12-08 00:33:58 +0800

    give default value to LogRecord date

--- src/main/scala/com/icbc/pdlp/LogRecord.scala
+++ src/main/scala/com/icbc/pdlp/LogRecord.scala
@@ -4,13 +4,13 @@ package com.icbc.pdlp
   * Created by ConnorWeng on 2015/12/1.
   */
 case class LogRecord(appId: String, mid: String, sid: String, timestamp: String, page: String,
-                     event: String, menu: String, other: String, date: String) {}
+                     event: String, menu: String, other: String, date: String = "") {}
 
 object LogRecord {
   implicit class String2LogRecord(line: String) {
     def mkLogRecord = {
       val parts = line.split(",", 8)
-      new LogRecord(parts(0), parts(1), parts(2), parts(3), parts(4), parts(5), parts(6), parts(7), "")
+      new LogRecord(parts(0), parts(1), parts(2), parts(3), parts(4), parts(5), parts(6), parts(7))
     }
   }
 }
